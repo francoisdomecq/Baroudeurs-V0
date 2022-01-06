05e03f6607b7c9616dc10078edc7dde74385748d
juliettemnr <meunierjuliette62@gmail.com>
2022-01-06 11:31:47 +0100

    description

--- Components/Details.js
+++ Components/Details.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { View, StyleSheet, Text, Image, Button } from 'react-native';
-import { MARKERS_DETAILS } from '../Data/Marker_Details';
 
 
 
@@ -9,18 +8,19 @@ function Details({route,navigation}){
     return (
       
   <View>
-          <View >
+          <View style={styles.detail}>
               <View>
-                  <Text>{marker.name}</Text>
+                  <Text style={styles.name}>{marker.name}</Text>
               </View>
-              <View>
+              <View style={styles.containerImage}>
                   <Image
-                      
+                      style ={styles.image}
                       source={marker.img}
                   ></Image>
               </View>
-              <View>
-                <Text>{marker.description} </Text>
+              <View style={styles.containerDescription} >
+                <Text style ={styles.title}>Histoire</Text>
+                <Text style={styles.description}>{marker.histoire} </Text>
               </View>
           </View>
       </View>
@@ -28,6 +28,36 @@ function Details({route,navigation}){
 }
 
 
+
 export default Details
+
+const styles = StyleSheet.create({
+  detail :{
+    textAlign:'center',
+    flexDirection:'column'
+  },
+  name: {
+      textAlign: 'center',
+      fontSize: 25,
+      fontWeight:'bold',
+  },
+
+  containerImage: {
+      marginVertical: 10,
+      alignItems: 'center',
+
+  },
+title:{
+  textAlign: 'center',
+      fontSize: 15,
+      fontWeight:'bold',
+
+},
+image:{
+  width: 300,
+  height: 220
+},
+
+});
 
 
--- Data/Markers_Data.js
+++ Data/Markers_Data.js
@@ -12,6 +12,7 @@ export const MARKERS_DATA = [
     typePI:"Espace extérieur",
     img: require('../assets/Places/Place_de_la_bourse.jpg'),
     imgmarker : require('../assets/Icon/PLACEBOURSE.png'),
+    histoire: "Comparée à la place Vendôme par Victor Hugo, la place de la Bourse est inaugurée en 1749 dans un but précis : servir de cadre pour arborer la statue équestre du roi Louis XV. Elle est successivement baptisée place Royale, place de la Liberté et place impériale avant d’adopter son nom actuel. La statue équestre, symbole de la monarchie, est abattue lors de la Révolution et remplacée par un symbole révolutionnaire : l’arbre de la liberté. C’est à cet emplacement que s’élève aujourd’hui majestueusement la fontaine des Trois Grâces, qui représente les filles de Zeus.",
 
   },
   {
@@ -26,6 +27,7 @@ export const MARKERS_DATA = [
     typePI:"Batiment historique",
     img:require('../assets/Places/GrandTheatre.jpg'),
     imgmarker : require('../assets/Icon/GRANDTHEATRE.png'),
+    histoire: "Depuis plus de deux siècles, l'histoire du Grand-Théâtre de Bordeaux s'écrit au rythme des événements qui ponctuent son existence.Le premier d'entre eux a lieu le 7 avril 1780, il s'agit de l'inauguration tant attendue du théâtre. Une foule immense se bouscule devant les portes du nouveau bâtiment. Certes, on vient assister à Athalie de Racine et au Jugement d'Apollon de Blincourt mais on vient surtout admirer la construction. Près de sept longues années se sont écoulées depuis le commencement des travaux commandés par le Gouverneur de Guyenne, Louis Armand du Plessis, duc de Richelieu. Le résultat est au rendez-vous. Là, sous les yeux du public, se dresse une véritable merveille. L'architecte Victor Louis a usé de tout son talent pour offrir harmonie, raffinement, sobriété, puissance, pureté, élégance, noblesse d'ordonnance à son œuvre... On ne tarit pas d'éloges, aujourd'hui encore, sur ce bâtiment qui demeure un véritable chef-d'œuvre de l'art architectural du XVIIIe siècle. À la valeur esthétique de l'ouvrage s'ajoute l'acoustique exceptionnelle de la salle due à la carcasse de bois qui l'enveloppe.",
   },
   {
     id: '3',
@@ -40,6 +42,7 @@ export const MARKERS_DATA = [
     typePI:"Musée",
     img:require('../assets/Places/MuseeAquitaine.jpg'),
     imgmarker : require('../assets/Icon/MUSEEAQUITAINE.png'),
+    histoire: "Dans sa conception actuelle, le musée d’Aquitaine a été officiellement créé en 1962 à l’initiative du muséologue Georges Henri Rivière qui avait été chargé de la réorganisation des musées d’histoire, archéologie et ethnologie après la guerre. Il avait proposé de mettre en place un réseau cohérent de musées à l’échelle nationale avec en particulier la création dans chaque capitale régionale d’un musée de synthèse qui retrace l’histoire de la région des origines à nos jours. Sa démarche était en rupture avec les anciens musées locaux qui présentaient souvent une histoire anecdotique et fragmentaire. Les nouveaux musées devaient à l’inverse s’appuyer sur la recherche scientifique et proposer une vision d’ensemble, cohérente, fondée sur des « programmes interdisciplinaires périodisés ». Le musée d’Aquitaine est issu de ces conceptions, de même que le musée de Normandie ou le musée de Bretagne. Dans un premier temps, le musée d’Aquitaine a partagé le bâtiment du musée des Beaux-arts dont il occupait l’aile nord. Dans les années 1970, il est décidé d’installer le musée d’Aquitaine cours Pasteur, dans les anciens locaux de la faculté des sciences et des lettres de Bordeaux construite à la fin du XIXe siècle par Pierre-Charles Durand, architecte de la ville, sur l’emplacement du couvent des Feuillants où Michel de Montaigne fut enterré en 1592. Une ambitieuse politique d’acquisition est alors menée en parallèle aux travaux de rénovation du bâtiment qui ont lieu de 1982 à 1986 et le nouveau musée ouvre ses portes au public en 1987. Sa création a donné lieu au rassemblement d’un grand nombre de collections et de plusieurs musées dispersés dans la ville qui ont connu une histoire complexe." ,
   },
   {
     id: '4',
@@ -54,6 +57,7 @@ export const MARKERS_DATA = [
     typePI:"Espace extérieur",
     img:require('../assets/Places/miroir_d_eau.jpg'),
     imgmarker : require('../assets/Icon/PLACEBOURSE.png'),
+    histoire: "Michel Corajoud, concepteur de la rénovation des 4,5 kilomètres des quais de la Garonne, explique avoir eu une inspiration vénitienne, en référence à la place Saint-Marc recouverte d'eau3. Les travaux ont été exécutés de mai 2005 à juillet 20064. L'esplanade est d'une surface totale de 5 850 m2, soit 3 850 m2 pour le miroir d'eau lui-même et 2 000 m2 pour la dalle située à sa périphérie4. Le miroir d'eau fonctionne entre 10 heures et 22 heures. Il présente un quart d'heure d'eau, cinq minutes d'arrêt puis cinq minutes de brouillard5. Un réservoir de 800 m3, situé sous la dalle, permet de stocker l'eau de la fontaine. L'eau est remontée en surface à l'aide de pompes, l'eau remplit une centaine de petits canaux disposés sous la dalle du miroir puis s'infiltre à travers les joints pour se répandre sur toute la surface du miroir. Après 15 minutes, un système d'électrovanne permet à l'eau de redescendre dans le réservoir. L'effet brouillard est alors déclenché grâce à 900 injecteurs disposés au milieu des dalles, ce brouillard peut monter à une hauteur de 2 mètres. Puis le cycle continue avec le retour de l'eau sur les dalles de granit4. Le site Landscape Architects Network a classé le miroir d'eau bordelais à la quatrième place des réalisations mondiales des places urbaines6.",
   },
   {
     id: '5',
@@ -68,6 +72,7 @@ export const MARKERS_DATA = [
     typePI:"Batiment historique",
     img: require('../assets/Places/PontDePierre.jpg'),
     imgmarker : require('../assets/Icon/PONTDEPIERRE.png'),
+    histoire: "Le pont de pierre est un pont à voûtes en maçonnerie franchissant la Garonne à Bordeaux. Il permet de relier le centre-ville au quartier de La Bastide, sur la rive droite. Le pont de pierre, construit sur ordre de Napoléon Ier entre 1810 et 1822, a été conçu par les ingénieurs Claude Deschamps et Jean-Baptiste Basilide Billaudel. 4 000 ouvriers y travaillèrent. D'une longueur de 487 mètres, l'ouvrage présente 17 arches construites sur 16 piles. Le pont, d'une largeur initiale de 14,6 mètres, est passé à une largeur de 19 mètres en 1954. Il est construit en pierre et en brique, avec la particularité de comporter des espaces vides intérieurs. Contrairement à une croyance populaire, il n'a jamais été décidé de construire le pont avec 17 arches pour correspondre au nombre de lettres dans Napoléon Bonaparte. À l'origine, le pont devait compter 19 arches, mais pour des questions budgétaires et architecturales, deux arches ont finalement été retirées du projet1. Le financement de l'ouvrage, à hauteur de 6,5 millions de francs fut mixte  : le négociant bordelais Pierre Balguerie-Stuttenberg créa, en 1818, la Compagnie du pont de Bordeaux pour lever les financements privés nécessaires à l'achèvement des travaux, pour un montant de 2 millions de francs. En contrepartie la Compagnie du pont bénéficiera d'un droit de péage. Celui-ci sera racheté par la ville de Bordeaux en 1863, ce qui permet le rattachement de La Bastide à Bordeaux au 1er janvier 1865. Le pont de pierre est inscrit monument historique depuis le 17 décembre 20022.",
   },
   {
     id: '6',
@@ -82,6 +87,7 @@ export const MARKERS_DATA = [
     typePI:"Batiment autre",
     img: require('../assets/Places/HotelDeVille.jpg'),
     imgmarker : require('../assets/Icon/HOTELDEVILLE.png'),
+    histoire: "Le palais Rohan est un palais bordelais construit pour l'archevêque Ferdinand-Maximilien-Mériade, prince de Rohan, entre 1771 et 1784. Hôtel de l’Archevêché jusqu’à la Révolution, hôtel du département puis siège du tribunal révolutionnaire en 1791, hôtel de la préfecture en 1800, palais impérial de Napoléon Ier en 1808 et palais royal en 1815 sous Louis XVIII, le palais Roha",
   },
 
   {
@@ -97,6 +103,7 @@ export const MARKERS_DATA = [
     typePI:"Bar/Restaurant",
     img: require('../assets/Places/CharlesDickens.jpg'),
     imgmarker : require('../assets/Icon/DICKENS.png'),
+    histoire: "Notre sympathique équipe anglophone organise chaque semaine plusieurs évènements  qui raviront les jeunes et les moins jeunes  : quiz, tournois de fléchettes, soirées spéciales jeux et bien sûr, tous les grands événements sportifs diffusés sur 4  grands écrans. L’atmosphère chaleureuse saura vous réchauffer les jours d’hiver, alors que notre terrasse ensoleillée sera THE PLACE TO BE au printemps et en été. À une centaine de mètres de la place de la Bourse et de son célèbre miroir d’eau, situé dans le quartier vivant et bohème de St Pierre, le Charles Dickens  vous permettra de vivre une expérience 100% british avec ses bières anglaises à la pression ou en bouteille provenant de notre brasserie dans le Bedfordshire en Angleterre. Le choix est grand : de la Wells Bombardier qui a été primée jusqu’à l’audacieuse Young’s Double Chocolate Stout et sans oublier l’unique arôme de la Wells Banana Bread Beer ou le goût sucré et subtil de la Young’s Waggle Dance !",
   },
   
 ];
